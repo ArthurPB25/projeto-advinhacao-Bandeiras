66080b2fea328669e698f3a76db8a2a804e189d0
elipec8 <felipe.e.cruz6@aluno.senai.br>
2025-03-24 16:38:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-async function getPost() {
+async function BandeiraAleatoria() {
     try {
       const response = await fetch('https://restcountries.com/v3.1/all?languages=portuguese');
       const data = await response.json();
-      const randomCountry = data[Math.floor(Math.random() * data.length)];
-      const bandeira=randomCountry['altSpellings'][0].toLowerCase()
-      console.log(`https://flagcdn.com/w320/${bandeira}.png`)//
+      const randomCountry = data[Math.floor(Math.random() * data.length)]; //vai gerar um número aleatório dentro da quantidade de países
+      const bandeira=randomCountry.cca2.toLowerCase() //pegando a sigla do país e convertendo em minúsculo 
+      document.getElementById('flag').src=`https://flagcdn.com/w320/${bandeira}.png` //trasnfora o src da tag img nesse link
+      console.log(randomCountry.name.common)
     } catch (error) {
       console.error('Erro:', error);
     }
   }
 
-  getPost();
+  BandeiraAleatoria();
