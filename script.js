04e9bd5f3e5af8b2837a46f820625d9965c82c0e
elipec8 <felipe.e.cruz6@aluno.senai.br>
2025-03-24 16:05:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ async function getPost() {
       const response = await fetch('https://restcountries.com/v3.1/all?languages=portuguese');
       const data = await response.json();
       const randomCountry = data[Math.floor(Math.random() * data.length)];
-      console.log(randomCountry.flags.png);
+      const bandeira=randomCountry['altSpellings'][0].toLowerCase()
+      console.log(`https://flagcdn.com/w320/${bandeira}.png`)//
     } catch (error) {
       console.error('Erro:', error);
     }
