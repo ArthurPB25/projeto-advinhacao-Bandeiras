ad05f24520264fc2962ec25f35253a42420ce6ff
elipec8 <felipe.e.cruz6@aluno.senai.br>
2025-03-24 15:20:46 -0300

    aleatoriedade nas bandeiras

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+async function getPost() {
+    try {
+      const response = await fetch('https://restcountries.com/v3.1/all?languages=portuguese');
+      const data = await response.json();
+      const randomCountry = data[Math.floor(Math.random() * data.length)];
+      console.log(randomCountry);
+      console.log(randomCountry['flags'])
+      const bandeira=randomCountry['altSpellings'][0].toLowerCase()
+      console.log(`https://flagcdn.com/w320/${bandeira}.png`)
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  }
 
+  getPost();
