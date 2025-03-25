e6dfd514e531d342b7546c480718987b724c235e
ArthurPB25 <arthurprimobrandao@gmail.com>
2025-03-25 09:38:32 -0300

    Atualizando script

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 let nomeCorreto = "";
 
-        async function carregarBandeira() {
-            const response = await fetch("https://restcountries.com/v3.1/all");
-            const countries = await response.json();
-            const randomCountry = countries[Math.floor(Math.random() * countries.length)];
-            
-            document.getElementById("flag").src = randomCountry.flags.png;
-            nomeCorreto = randomCountry.name.common;
-        }
+async function BandeiraAleatoria() {
+    try {
+      const response = await fetch('https://restcountries.com/v3.1/all');
+      const data = await response.json();
+      const randomCountry = data[Math.floor(Math.random() * data.length)]; //vai gerar um número aleatório dentro da quantidade de países
+      const bandeira=randomCountry.cca2.toLowerCase() //pegando a sigla do país e convertendo em minúsculo 
+      document.getElementById('flag').src=`https://flagcdn.com/w320/${bandeira}.png` //trasnfora o src da tag img nesse link
+      nomeCorreto = randomCountry.translations.por.common
+      console.log(randomCountry.translations.por.common)
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  }
 
-        async function verificarResposta() {
-            const respostaJogador = document.getElementById("answer").value;
+  async function verificarResposta() {
+    const respostaJogador = document.getElementById("answer").value;
 
-            const response = await fetch("http://localhost:1880/verificar-resposta", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ resposta: respostaJogador, correta: nomeCorreto })
-            });
+    const response = await fetch("http://localhost:1880/verificar-resposta", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ resposta: respostaJogador, correta: nomeCorreto }) //faz o metodo post para a verificação
+    });
 
-            const data = await response.json();
-            document.getElementById("result").innerText = data.resultado;
-        }
+    const data = await response.json();
+    document.getElementById("result").innerText = data.resultado;
+}
 
-        carregarBandeira();
-
+  BandeiraAleatoria();
