52d8ee7bed1d20534670a9d8533f1b89fe4baecd
ArthurPB25 <arthurprimobrandao@gmail.com>
2025-04-29 08:46:07 -0300

    Reparos finais

--- main/script.js
+++ main/script.js
@@ -1,3 +1,4 @@
+// Salva o nome na URL
 const urlParams = new URLSearchParams(window.location.search);
 let nickname = urlParams.get('nickname') || '';
 
@@ -15,44 +16,6 @@ let startTime = null;
 let intervalId = null;
 let totalTime = 0;
 
-// Função para salvar o nome do jogador
-async function salvarNome() {
-    nickname = document.getElementById('nickname').value.trim().toLowerCase();
-    console.log("Nome digitado:", nickname);
-
-    if (!nickname) {
-        alert("Por favor, digite seu nome antes de iniciar!");
-        return;
-    }
-
-    const response = await fetch("http://10.106.208.41:1880/guardar-nome", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ nome: nickname, ponto: 0 })
-    });
-
-    const data = await response.json();
-    console.log(data);
-    document.getElementById('jogador').innerText = data.nome;
-}
-
-
-
-// Função para iniciar o jogo
-async function startGame() {
-    nickname = document.getElementById('nickname').value.trim().toLowerCase();
-    
-    if (!nickname) {
-        alert("Por favor, digite seu nome antes de iniciar!");
-        return;
-    }
-    
-    await salvarNome(); // Primeiro salva o nome no servidor
-    alert('Iniciando o jogo para: ' + nickname);
-
-    // Redireciona passando o nickname pela URL
-    window.location.href = "index.html?nickname=" + encodeURIComponent(nickname);
-}
 
 
 // Função para obter uma bandeira aleatória
@@ -157,7 +120,7 @@ async function verificarResposta() {
         resta.innerText = `Restam: ${11 - cont}`;
     } else {
         await salvarTempoFinal();
-        window.location.href = 'ranking.html';
+        window.location.href = '../ranking/ranking.html';
     }
 }
 
---
+++ ranking/teste.js
@@ -0,0 +1,20 @@
+async function ranking() {
+    const response = await fetch("http://10.106.208.41:1880/pegar-nome", { method: "GET" });
+    const data = await response.json();
+    console.log(data); // Conferir a estrutura dos dados
+
+    let html = '';
+
+    // Transformar o objeto em um array de valores e ordenar por pontos (decrescente)
+    const rankingArray = Object.values(data).sort((a, b) => b.ponto - a.ponto);
+
+    // Gerar o HTML com os dados ordenados
+    rankingArray.forEach(item => {
+        html += `<p>${item.nome} - Pontos: ${item.ponto} - Tempo: ${item.tempo}</p>`;
+    });
+
+    // Exibir o ranking na página
+    document.getElementById('ranking').innerHTML = html;
+}
+
+ranking();
