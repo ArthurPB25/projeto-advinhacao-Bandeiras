924febe03d22087c7497a1c04174c3fb5a468a62
ArthurPB25 <arthurprimobrandao@gmail.com>
2025-05-05 08:16:39 -0300

    mudando protocolo

--- ranking/teste.js
+++ ranking/teste.js
@@ -1,5 +1,5 @@
 async function ranking() {
-    const response = await fetch("http://10.106.208.41:1880/pegar-nome", { method: "GET" });
+    const response = await fetch("https://10.106.208.41:1880/pegar-nome", { method: "GET" });
     const data = await response.json();
     console.log(data); // Conferir a estrutura dos dados
 
