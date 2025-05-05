0f46bf97d4251ab18d828aeded0a722e8b8d707e
ArthurPB25 <arthurprimobrandao@gmail.com>
2025-05-05 14:32:55 -0300

    teste

--- pagina_inicial/script.js
+++ pagina_inicial/script.js
@@ -8,7 +8,7 @@ async function salvarNome() {
         return;
     }
 
-    const response = await fetch("https://f0a8-200-211-208-194.ngrok-free.app/guardar-nome", {
+    const response = await fetch("https://10.106.208.41:1880/guardar-nome", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ nome: nickname, ponto: 0 })
